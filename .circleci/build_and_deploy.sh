fc962704e579aa03af54a1f53edf55f908160882
Peinan ZHANG <zhang_peinan@cyberagent.co.jp>
2018-06-27 15:17:23 +0900

    fixed script

--- .circleci/build_and_deploy.sh
+++ .circleci/build_and_deploy.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-## set up docker container
+## set up variables
 IMAGE=mecab-server
 DATE=$(date +"%Y-%m-%d-%H%M%S")
+gcloud=/opt/google-cloud-sdk/bin/gcloud
 
 ## set up gcp
 echo $ACCT_AUTH | base64 -d > $HOME/gcp-key.json
-sudo gcloud components update
-sudo gcloud auth activate-service-account --key-file $HOME/gcp-key,json
-sudo gcloud config set project $GCP_PROJECT_ID
+sudo $gcloud components update
+sudo $gcloud auth activate-service-account --key-file $HOME/gcp-key,json
+sudo $gcloud config set project $GCP_PROJECT_ID
 
 # build
 docker build . \
@@ -17,6 +18,5 @@ docker build . \
   -t $GCR_URL/$GCP_PROJECT_ID/peinan/$IMAGE
 
 ## deploy to GCR and GAE
-sudo gcloud 
-sudo gcloud app deploy
-sudo gcloud app browse -s $IMAGE
+sudo $gcloud app deploy
+sudo $gcloud app browse -s $IMAGE
