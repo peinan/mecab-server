085ee9bd518f5ee6bf03ac25462cbc8270618ed1
Peinan ZHANG <zhang_peinan@cyberagent.co.jp>
2018-06-27 16:01:23 +0900

    fixed script

--- .circleci/build_and_deploy.sh
+++ .circleci/build_and_deploy.sh
@@ -4,22 +4,27 @@
 IMAGE=mecab-server
 DATE=$(date +"%Y-%m-%d-%H%M%S")
 CMD_GCLOUD=/opt/google-cloud-sdk/bin/gcloud
-GCR_URL=$GCR_DOMAIN/$GCP_PROJECT_ID/peinan/$IMAGE
+IMAGE_URL=$GCR_DOMAIN/$GCP_PROJECT_ID/peinan/$IMAGE
 
 ## set up gcp
-echo $ACCT_AUTH | base64 -d > $HOME/gcp-key.json
+echo $GCR_AUTH | base64 -d > $HOME/gcr-key.json
 sudo $CMD_GCLOUD components update
-sudo $CMD_GCLOUD auth activate-service-account --key-file $HOME/gcp-key.json
+sudo $CMD_GCLOUD auth activate-service-account --key-file $HOME/gcr-key.json
 sudo $CMD_GCLOUD config set project $GCP_PROJECT_ID
 
 # build
 docker build \
   -t $IMAGE:$DATE \
   -t $IMAGE:latest \
-  -t $GCR_URL:latest \
+  -t $IMAGE_URL:latest \
   .
 
-## deploy to GCR and GAE
-sudo $CMD_GCLOUD docker -- push $GCR_URL
-sudo $CMD_GCLOUD app deploy
+## deploy to GCR
+sudo $CMD_GCLOUD docker -- push $IMAGE_URL
+
+## deploy to GAE
+echo $GAE_AUTH | base64 -d > $HOME/gae-key.json
+sudo $CMD_GCLOUD auth activate-service-account --key-file $HOME/gae-key.json
+sudo $CMD_GCLOUD config set project $GCP_PROJECT_ID
+sudo $CMD_GCLOUD app deploy --image-url $IMAGE_URL
 sudo $CMD_GCLOUD app browse -s $IMAGE
